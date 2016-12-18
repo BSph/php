4aad7661a4040301c5ad13fda33d16d16d1f5238
BSph <sboly21@gmail.com>
2016-12-18 01:26:56 +0100

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,47 @@
+<div id="carousel-example-2" class="carousel slide carousel-fade z-depth-1-half" data-ride="carousel">
+                                <!--Indicators-->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+                                    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+                                </ol>
+                                <!--/.Indicators-->
+
+                                <!--Slides-->
+                                <div class="carousel-inner" role="listbox">
+                                    <div class="carousel-item active">
+                                        <img src="img/slides/s12.jpg" alt="First slide">
+                                        <div class="carousel-caption">
+                                            <h4>Formations valorisantes pour tous</h4>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/slides/s11.jpg" alt="Second slide">
+                                        <div class="carousel-caption">
+                                            <h4>Enseignement modulaire</h4>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="img/slides/s10.jpg" alt="Third slide">
+                                        <div class="carousel-caption">
+                                            <h4>Horaires décalés</h4>
+                                            <br>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--/.Slides-->
+
+                                <!--Controls-->
+                                <a class="left carousel-control" href="#carousel-example-2" role="button" data-slide="prev">
+                                    <span class="icon-prev" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-example-2" role="button" data-slide="next">
+                                    <span class="icon-next" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                                <!--/.Controls-->
+                            </div>
+                            <!--/.Carousel Wrapper-->
